141eecf883bd3305372b4a105eb9e380a8d4f89c
Adriean Khisbe <adriean.khisbe@live.fr>
2014-07-02 00:35:02 +0200

    Diverses annotations

--- ApiVelib.rb
+++ ApiVelib.rb
@@ -2,13 +2,16 @@
 require 'net/http'
 require 'net/https' # pas nécessaire
 require 'json'
-# §todo logging
+# §todo logging 𝄞
 
 class ApiVelib
+
+  API_KEY = "c9ce7179fe009f45d27565e5de702fab6da12dcd"
+  # #maybe:in conf file?
+
   def initialize
     @api = "https://api.jcdecaux.com"
-    @params = { :contract => "paris",
-      :apiKey => API_KEY }
+    @params = { :contract => "paris", :apiKey => API_KEY }
     @base_uri = URI.parse("#{@api}/vls/v1/stations")
     self.connect_api
   end
@@ -18,27 +21,26 @@ class ApiVelib
     @http = Net::HTTP.new(@base_uri.host, 443)  # doit être parsée
     @http.use_ssl = true
     @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    puts "Connected to VelibApi"
+    puts "Connected to VelibApi" # log
   end
 
 
   public
   def get_station(station_number)
     ## récupère données
-    uri = URI.parse("#{@base_uri}/#{station_number}")
+    uri = URI.parse("#{@base_uri}/#{station_number}") # voir autres addresses api fonction helper
     uri.query = URI.encode_www_form(@params)
-    apirequest = Net::HTTP::Get.new(uri.request_uri)  ## bug
-    resp = @http.request(apirequest)
+    api_request = Net::HTTP::Get.new(uri.request_uri)  ## bug
+    resp = @http.request(api_request)
+    # §see: multirequest
 
     ## affiche station
     if resp.is_a?(Net::HTTPSuccess)
       data = JSON.parse(resp.body, symbolize_names: true)
-      # puts data
-      ## print all:    data.each{|k,v| puts "#{k}: #{v}"}
+      # puts data # log
       return Station.new(data)
     else
-      puts "Damn ErrorOccured: #{resp}"
-      nil
+      raise "Damn ErrorOccured: #{resp}"
     end
   end
 end
--- velibe.rb
+++ velibe.rb
@@ -5,8 +5,6 @@
 require_relative 'ApiVelib' # why?
 require_relative 'Station'
 
-API_KEY = "c9ce7179fe009f45d27565e5de702fab6da12dcd"
-
 # Default values
 stations = [10035, 19003, 19004, 10031]
 
@@ -14,6 +12,10 @@ stations = [10035, 19003, 19004, 10031]
 api = ApiVelib.new
 #todo: try catch network exception
 # §tosingleton!!
+# §maybe: inversion de controle? c'est station, initialisée avec le numéro
+# qui appelle l'api plutot que api qui controle station?
+# (rendrait transparent appel à station)
+# (méthode update & update!)
 
 if ARGV.length == 0
   puts "Station par défault:"  # see logging
