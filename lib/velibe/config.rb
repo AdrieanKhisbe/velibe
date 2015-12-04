425a09b01957ed0d724edec6bcc4d1e518eabfe3
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-04 05:55:39 +0100

    refactor: config now depends on VELIBE_DB_PATH and VELIBE_CONFIG_PATH ENV variables

--- lib/velibe/config.rb
+++ lib/velibe/config.rb
@@ -2,15 +2,14 @@ module Velibe
   module Config
     #  §see: http://stackoverflow.com/questions/7828066/accessing-files-packaged-into-a-ruby-gem
 
-    DB_NAME = '~/.velib.db' # TODO: more generic + config
+    DB_NAME = ENV['VELIBE_DB_PATH'] || '~/.velib.db'
     DB_PATH = Pathname.new(DB_NAME).expand_path # .to_s?
     DATA_CSV = '../../data/Paris.csv'
 
     DATA_CSV_FILE = File.join(File.dirname(File.expand_path(__FILE__)), DATA_CSV)
 
-    KV_NAME = '~/.velib.yaml' # TODO: more generic
+    KV_NAME =  ENV['VELIBE_CONFIG_PATH']|| '~/.velib.yaml'
     KV_PATH = Pathname.new(KV_NAME).expand_path
-
 
   end
 end
